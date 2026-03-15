d7c9dbee216551066fbb2ad9ab18000284ed820b
Shweta <raishweta048@gmail.com>
2026-03-15 11:07:50 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const connectDB = require("./config/db")
+const rateLimit = require("express-rate-limit")
+
+dotenv.config()
+
+const app = express()
+
+connectDB()
+
+app.use(express.json())
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 20,
+  message: "Too many requests, please try again later."
+})
+
+app.use(limiter)
+
+const productRoutes = require("./routes/productRoutes")
+
+app.use("/api/products", productRoutes)
+
+const errorHandler = require("./middleware/errorHandler")
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
